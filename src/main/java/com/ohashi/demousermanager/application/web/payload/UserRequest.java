0190f8ab0f039a57d8d6983e4617ad2ef93a9c5c
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-08-14 17:08:42 -0300

    fix: add annotation to validate payload

--- src/main/java/com/ohashi/demousermanager/application/web/payload/UserRequest.java
+++ src/main/java/com/ohashi/demousermanager/application/web/payload/UserRequest.java
@@ -3,17 +3,36 @@ package com.ohashi.demousermanager.application.web.payload;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
+@Validated
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRequest {
 
+    @NotNull
+    @NotEmpty
     private String name;
 
+    @NotNull
+    @NotEmpty
+    @NotBlank
     private String nickname;
 
+    @NotNull
+    @NotEmpty
+    @NotBlank
     private String password;
 
+    @Email
+    @NotNull
+    @NotEmpty
+    @NotBlank
     private String email;
 }
